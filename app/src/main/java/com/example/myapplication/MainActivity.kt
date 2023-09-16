4ffbdbec2513e291706348ae0198fbd08c1946df
JR-Bam <110076336+JR-Bam@users.noreply.github.com>
2023-09-16 22:17:23 +0800

    Lists - Android Jetpack Compose - Part 8

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -3,32 +3,22 @@ package com.example.myapplication
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SnackbarHost
-import androidx.compose.material3.SnackbarHostState
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Text
-import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
-import kotlinx.coroutines.launch
-
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
-    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -39,49 +29,22 @@ class MainActivity : ComponentActivity() {
         )
 
         setContent {
-            val scaffoldState = remember {SnackbarHostState()}
-            var textFieldState by remember {
-                mutableStateOf("")
-            }
 
-            val scope = rememberCoroutineScope()
-
-            Scaffold(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(20.dp),
-                snackbarHost = { SnackbarHost(hostState = scaffoldState) }
-            ) { contentPadding ->
-                Column(
-                    modifier = Modifier
-                        .padding(contentPadding)
-                        .padding(30.dp)
-                        .fillMaxSize(),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.Center
-                ) {
-                    OutlinedTextField(
-                        value = textFieldState,
-                        label = {
-                            Text("Enter your name", fontFamily = fontFamily)
-                        },
-                        onValueChange = {
-                            textFieldState = it
-                        },
-                        singleLine = true,
-                        modifier = Modifier.fillMaxWidth()
+            LazyColumn {
+                items (50){
+                    Text(
+                        text = "Text $it",
+                        fontSize = 24.sp,
+                        fontFamily = fontFamily,
+                        fontWeight = FontWeight.Bold,
+                        textAlign = TextAlign.Center,
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(vertical = 24.dp)
                     )
-                    Spacer(modifier = Modifier.height(15.dp))
-                    Button(onClick = {
-                        scope.launch {
-                            scaffoldState.showSnackbar("Hello $textFieldState")
-                        }
-                    }) {
-                        Text("Please greet me.")
-                    }
                 }
-            }
 
+            }
         }
     }
 }
